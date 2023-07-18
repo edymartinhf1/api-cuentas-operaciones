96a95f72a1388f63ef903885359ef8b456caa271
edymartinhf1 <edymartinhf1@gmail.com>
2023-07-17 19:48:28 -0500

    sonar fix

--- src/main/java/com/bootcamp/bank/operaciones/exception/BusinessException.java
+++ src/main/java/com/bootcamp/bank/operaciones/exception/BusinessException.java
@@ -4,11 +4,9 @@ package com.bootcamp.bank.operaciones.exception;
  * Clase Exception de Negocio
  */
 public class BusinessException extends RuntimeException{
-    private String messageError;
 
     public BusinessException(String messageError) {
         super(messageError);
-        this.messageError = messageError;
     }
 
 
--- src/test/java/com/bootcamp/bank/ApiCuentasOperacionesApplicationTests.java
+++ src/test/java/com/bootcamp/bank/ApiCuentasOperacionesApplicationTests.java
@@ -1,10 +1,13 @@
 package com.bootcamp.bank;
 
+import com.bootcamp.bank.operaciones.model.dao.OperacionCtaDao;
 import com.bootcamp.bank.operaciones.service.ComisionesService;
 import com.bootcamp.bank.operaciones.service.OperacionCuentaService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -18,6 +21,14 @@ class ApiCuentasOperacionesApplicationTests {
 	void contextLoads() {
 		assertThat(comisionesService).isNotNull();
 		assertThat(operacionCuentaService).isNotNull();
+	}
+
+	@Test
+	void findAllAccount() {
+		// When
+		List<OperacionCtaDao> operaciones = this.operacionCuentaService.findAll().collectList().block();
+		// Then
+		assertThat(operaciones.size()>0);
 	}
 
 }
