1a35cc2055ececfd4b8d66b3505c9057a3b1a372
edymartinhf1 <edymartinhf1@gmail.com>
2023-07-24 14:57:24 -0500

    actualizacion discovery

--- src/main/java/com/bootcamp/bank/ApiCuentasOperacionesApplication.java
+++ src/main/java/com/bootcamp/bank/ApiCuentasOperacionesApplication.java
@@ -2,10 +2,12 @@ package com.bootcamp.bank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan({"com.bootcamp.bank.operaciones.*"})
+@EnableEurekaClient
 public class ApiCuentasOperacionesApplication {
 
 	public static void main(String[] args) {
